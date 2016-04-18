3b7b124f0cb87a38ab0c8bbb60def03306ce8341
Mayur-Dev <mayurchoksi@gmail.com>
2016-04-19 05:58:24 +1000

    added headers and timeout

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 var soap = require('soap');
 var parser = require('node-xml2json');
 var url = 'http://www.webservicex.com/globalweather.asmx?WSDL';
-var args = {CityName: 'Melbourne Airport', CountryName: 'Australia'};
 soap.createClient(url, function(err, client) {
-    client.GetWeather(args, function(err, result) {
+
+    var payload = {CityName: 'Melbourne Airport', CountryName: 'Australia'};
+    //add soap headers
+    //var soapHeader = {
+    //  'AuthenticationToken': process.argv[2],
+    //  'DeveloperToken': process.argv[3],
+    //  'CustomerId': process.argv[4],
+    //  'CustomerAccountId': process.argv[5]
+    //};
+    //client.addSoapHeader(soapHeader);
+
+    //add client security
+    //client.setSecurity(new soap.BasicAuthSecurity('username', 'password'));
+
+    //ws-security
+    //client.setSecurity(new soap.WSSecurity('username', 'password'))
+
+    //ws-security with x509 certificate
+    //var privateKey = fs.readFileSync(privateKeyPath);
+    //var publicKey = fs.readFileSync(publicKeyPath);
+    //var password = ''; // optional password
+    //var wsSecurity = new soap.WSSecurityCert(privateKey, publicKey, password, 'utf8');
+    //client.setSecurity(wsSecurity);
+
+    //bearer security
+    //client.setSecurity(new soap.BearerSecurity('token'));
+
+    client.GetWeather(payload, function(err, result) {
       var getWeatherResult = parser.parser(result.GetWeatherResult);
       console.dir(getWeatherResult);
-    });
+    },{timeout: 5000},{time: true});
 
-    args = {CountryName: 'Australia'};
-    client.GetCitiesByCountry(args, function(err, result) {
+    payload = {CountryName: 'Australia'};
+    client.GetCitiesByCountry(payload, function(err, result) {
         var citiesByCountry = parser.parser(result.GetCitiesByCountryResult);
-        console.dir(citiesByCountry);
-    });
+        //console.dir(citiesByCountry);
+    },{timeout: 5000},{time: true});
 });
